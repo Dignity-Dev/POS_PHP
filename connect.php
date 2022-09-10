2cf206643236d41e58538f00e85c39ece907db70
Basheer Haadi A <47453288+Dignity-Dev@users.noreply.github.com>
2022-09-10 10:27:32 +0100

    update connect

--- connect.php
+++ connect.php
@@ -6,7 +6,7 @@ $db_pass		= '';
 $db_database	= 'sales'; 
 
 /* End config */
-
+// untill connect
 $db = new PDO('mysql:host='.$db_host.';dbname='.$db_database, $db_user, $db_pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
